3b3f60255148627310c82a1c3a1673da38107851
Sara Hajbane <sara.hajbane@gmail.com>
2025-03-20 22:24:28 +0100

    prime eds

--- prime numbers/prime.py
+++ prime numbers/prime.py
@@ -8,31 +8,36 @@ Also includes a function to check if any number is divisible by any another numb
 """
 
 def is_divisible_by(number, by):
-    if number % by == 0:
-        return True
-    else:
-        return False  
+    return number % by == 0
+
 
 def is_prime(number):
-    if number <= 2:
+    if number == 2:
         return True
+    
     for i in range(2,number):
         if is_divisible_by(number,i) == True:
             return False
+        
     return True
+
 
 def primes_in_range(start, end):
     primes = []
+
     for i in range(start, end):
         if is_prime(i):
             print(f"The number {i} is prime")
             primes.append(i)
+
     return primes
+
 
 def main():
     start = int(input("Please enter the start of the range you want to check:"))
     end = int(input("Please enter the end of the range you want to check:"))
     primes_in_range(start, end)
+
 
 if __name__ == "__main__":
     main()
